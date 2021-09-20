004922dbc516abfbcdd67f9a961d3f3b6cb15d58
Sjuul Janssen <sjuul@outlook.com>
2021-09-20 08:03:32 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+
+
+module "aws_services" {
+  source = "git@github.com:kabisa/terraform-datadog-service-check-monitor.git?ref=1.2.0"
+
+  name       = "Service Status"
+  check_name = "aws.status"
+
+  alert_message    = "The AWS service {{service}} is not reporting correctly to Datadog."
+  recovery_message = "The AWS service {{service}} integration has recovered"
+
+  by_tags             = var.by_tags
+  include_tags        = var.include_tags
+  exclude_tags        = var.exclude_tags
+  require_full_window = false
+  no_data_timeframe   = var.no_data_timeframe
+  notify_no_data      = var.notify_no_data
+
+  # monitor level vars
+  enabled            = var.enabled
+  alerting_enabled   = var.alerting_enabled
+  critical_threshold = var.critical_threshold
+  warning_threshold  = var.warning_threshold
+  ok_threshold       = var.ok_threshold
+  priority           = var.priority
+  docs               = var.docs
+  note               = var.note
+
+  # module level vars
+  env                  = var.env
+  service              = var.service
+  notification_channel = var.notification_channel
+  additional_tags      = var.additional_tags
+  locked               = var.locked
+  name_prefix          = var.name_prefix
+  name_suffix          = var.name_suffix
+}
+
